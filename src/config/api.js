731a54a72700a7282e1ddd3a874ffb20d6981e86
Redi <r.ibra.ext@teamsystem.com>
2023-08-07 10:43:05 +0200

    removed api keys

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,12 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "63e7733078msh8f8d0f16ece3e11p17020ejsnb3df5cec3dd8",
+    "X-RapidAPI-Key": "Your api key here",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
-export const Weather_API_KEY = "8d1945485b44fa24160b23da6b7e2197";
+export const Weather_API_KEY = "Your api key here";
+//Use your openweather account to access this api
