4084383e8a0546aeea7cc29d0aec5b09fd590ca7
Redi <redi_ibra04@outlook.com>
2023-08-10 15:27:36 +0200

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -5,6 +5,7 @@ export const geoApiOptions = {
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
+
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
