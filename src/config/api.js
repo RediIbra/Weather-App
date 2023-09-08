1bd1c101ac2b9e48657c6f7d65d97c5c5f4e5f93
Redi Ibra <r.ibra@teamsystem.com>
2023-09-08 11:42:51 +0200

    added some coments

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
+    /// Put geographic key api
     "X-RapidAPI-Key": "Your api key here",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
@@ -10,4 +11,6 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
 export const Weather_API_KEY = "Your api key here";
+// Use weather api key here
+
 //Use your openweather account to access this api
